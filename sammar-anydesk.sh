16e7a09a6446338249de1ea1977175b54c29d9d9
Gitone13 <136300713+Gitone13@users.noreply.github.com>
2023-06-19 17:02:02 +0530

    Update sammar-anydesk.sh

--- sammar-anydesk.sh
+++ sammar-anydesk.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && upgrade -y
 
-wget https://github.com/Gitone13/Dependencies/blob/main/myanydesk.deb
+wget https://github.com/Gitone13/Dependencies/blob/main/myanydesk.deb .sh
 
 sudo dpkg-i myanydesk.deb
  
