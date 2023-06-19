b48a8410879351a557c72192dfd8979d801aa045
Gitone13 <136300713+Gitone13@users.noreply.github.com>
2023-06-19 15:26:23 +0530

    Update sammar-anydesk.sh

--- sammar-anydesk.sh
+++ sammar-anydesk.sh
@@ -4,8 +4,8 @@ sudo apt update && upgrade -y
 
 wget https://github.com/Gitone13/Dependencies/blob/main/sammar_6.2.1-1_amd64.deb
 
-sudo dpkg -i sammar_6.2.1-1_amd64.deb
-
+sudo dpkg-deb --install sammar_6.2.1-1_amd64.deb
+ 
 sudo rm sammar_6.2.1-1_amd64.deb
 
 sudo apt --fix-broken install -y
